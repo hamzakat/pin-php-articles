e37c7ad67d4eeaac72d3b3d213a60360318283a9
Hamza <h@local>
2022-02-25 23:25:36 +0300

    Split the head and footer into two template files

--- index.php
+++ index.php
@@ -1,72 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require("layout/head.php") ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Articles</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src="tailwind.config.js"></script>
-
-</head>
-
-<body>
-    <section class="text-gray-600 body-font mt-5">
-        <div class="container px-5 py-5 mx-auto">
-            <div class="flex flex-col text-center w-full mb-5">
-                <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Made by <a class="font-bold" href="https://github.com/hamzakat">Hamza Kattan</a></h2>
-                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">PIN Articles</h1>
-                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">My submission for <a class="text-indigo-500" href="https://github.com/humansis/php-assignment">https://github.com/humansis/php-assignment</a></p>
-            </div>
-
-            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Add Article</button>
+<section class="text-gray-600 body-font mt-5">
+    <div class="container px-5 py-5 mx-auto">
+        <div class="flex flex-col text-center w-full mb-5">
+            <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Made by <a class="font-bold" href="https://github.com/hamzakat">Hamza Kattan</a></h2>
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">PIN Articles</h1>
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">My submission for <a class="text-indigo-500" href="https://github.com/humansis/php-assignment">https://github.com/humansis/php-assignment</a></p>
         </div>
-    </section>
-    <section class="text-gray-600 body-font overflow-hidden">
-        <div class="container px-5 py-24 mx-auto">
-            <div class="-my-8 divide-y-2 divide-gray-100">
-                <div class="py-8 flex flex-wrap md:flex-nowrap">
-                    <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                        <span class="font-semibold title-font text-gray-700">Author</span>
-                        <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-                    </div>
-                    <div class="md:flex-grow">
-                        <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Title</h2>
-                        <p class="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis praesentium harum nobis nulla molestias vel, illum atque assumenda dolorem, cupiditate quo, nemo voluptates recusandae ratione asperiores numquam nam. Quam, rerum?</p>
-                        <a class="text-indigo-500 inline-flex items-center mt-4">Read More
-                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M5 12h14"></path>
-                                <path d="M12 5l7 7-7 7"></path>
-                            </svg>
-                        </a>
-                    </div>
+
+        <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Add Article</button>
+    </div>
+</section>
+<section class="text-gray-600 body-font overflow-hidden">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="-my-8 divide-y-2 divide-gray-100">
+            <div class="py-8 flex flex-wrap md:flex-nowrap">
+                <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                    <span class="font-semibold title-font text-gray-700">Author</span>
+                    <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
                 </div>
-
+                <div class="md:flex-grow">
+                    <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Title</h2>
+                    <p class="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis praesentium harum nobis nulla molestias vel, illum atque assumenda dolorem, cupiditate quo, nemo voluptates recusandae ratione asperiores numquam nam. Quam, rerum?</p>
+                    <a class="text-indigo-500 inline-flex items-center mt-4">Read More
+                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M5 12h14"></path>
+                            <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                    </a>
+                </div>
             </div>
+
         </div>
-    </section>
-    <footer class="text-gray-600 body-font">
-        <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-            <a>
-                <img src="https://www.humansis.org/wp-content/uploads/2019/10/logo-humansis-1.png" class="w-20 " viewBox="0 0 24 24">
+    </div>
+</section>
 
-            </a>
-            <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">2022 Hamza
-            </p>
-            <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                <a class="text-gray-500" href="https://github.com/hamzakat/pin-php-articles">
-                    <svg width="24" height="24" fill="currentColor" class=" mr-3 text-opacity-50 transform">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.606 9.606 0 0112 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48C19.137 20.107 22 16.373 22 11.969 22 6.463 17.522 2 12 2z">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.606 9.606 0 0112 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48C19.137 20.107 22 16.373 22 11.969 22 6.463 17.522 2 12 2z"></path>
-                    </svg>
-
-
-                </a>
-
-            </span>
-        </div>
-    </footer>
-</body>
-
-</html>
+<?php require("layout/footer.php") ?>
