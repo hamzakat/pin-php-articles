51abac888a7ddb0e9b3797a47aa3602100cde4f8
Hamza <h@local>
2022-02-26 01:17:44 +0300

    Add "Back to home" button to head and change the index.php styles

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require("layout/head.php");
 
 <section class="text-gray-600 body-font overflow-hidden">
     <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"><a href="/add-article.php">Add Article</a></button>
-    <div class="container px-5 py-24 mx-auto">
+    <div class="container px-5 py-24 lg:w-2/ md:w-3/4 mx-auto">
         <div class="-my-8 divide-y-2 divide-gray-100">
             <div class="py-8 flex flex-wrap md:flex-nowrap">
                 <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
@@ -17,7 +17,7 @@ require("layout/head.php");
                 <div class="md:flex-grow">
                     <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Title</h2>
                     <p class="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis praesentium harum nobis nulla molestias vel, illum atque assumenda dolorem, cupiditate quo, nemo voluptates recusandae ratione asperiores numquam nam. Quam, rerum?</p>
-                    <a class="text-indigo-500 inline-flex items-center mt-4">Read More
+                    <a href="article.php" class="text-indigo-500 inline-flex items-center mt-4">Read More
                         <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M5 12h14"></path>
                             <path d="M12 5l7 7-7 7"></path>
--- layout/head.php
+++ layout/head.php
@@ -14,16 +14,29 @@
 <body>
 
     <section class="text-gray-600 body-font mt-5">
-        <div class="container px-5 py-5 mx-auto">
-            <div class="flex flex-col text-center w-full mb-5">
-                <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Made by <a class="font-bold" href="https://github.com/hamzakat/pin-php-articles">Hamza Kattan</a></h2>
-                <h1 class="sm:text-3xl text-2xl font-medium title-font my-4 text-gray-900"> <?php echo isset($PAGE_TITLE) ? $PAGE_TITLE : "Title" ?> </h1>
-                <?php
-                if (isset($DESCRIPTION)) {
-                    echo "<p class=\"lg:w-2/3 mx-auto leading-relaxed text-base\">" . $DESCRIPTION . "</p>";
-                }
-                ?>
 
+        <?php
+        if ($PAGE_TITLE != "Articles") { ?>
+            <div class="container lg:w-2/ md:w-3/4 px-5 mb-10 mx-auto">
+                <a href="index.php" class="text-indigo-500 inline-flex items-center mt-4">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                    </svg>
+                    Back to homepage
+                </a>
             </div>
+        <?php } ?>
+
+        <div class="flex flex-col text-center w-full mb-5">
+            <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Made by <a class="font-bold" href="https://github.com/hamzakat/pin-php-articles">Hamza Kattan</a></h2>
+            <h1 class="sm:text-3xl text-2xl font-medium title-font my-4 text-gray-900"> <?php echo isset($PAGE_TITLE) ? $PAGE_TITLE : "Title" ?> </h1>
+            <?php
+            if (isset($DESCRIPTION)) {
+                echo "<p class=\"lg:w-2/3 mx-auto leading-relaxed text-base\">" . $DESCRIPTION . "</p>";
+            }
+            ?>
+
+        </div>
         </div>
     </section>
